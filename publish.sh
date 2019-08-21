eb80410e342bdd4e5f11e94cae724cd9fd2dc55b
erichards52 <edward.richards5252@gmail.com>
2019-08-21 12:50:00 +0100

    updated publish.sh

--- publish.sh
+++ publish.sh
@@ -5,3 +5,21 @@ for ((i=0; i < ${#html_files[@]}; i+=n)); do
   git commit -m "Iteratively adding HTML files"
   git push origin master --force
 done
+
+BTK_img_files=(BTK_Outputs/img/*.png) # use your own glob expression
+n=200               # number of files per command line; adjust to taste
+for ((i=0; i < ${#BTK_img_files[@]}; i+=n)); do
+  git add "${BTK_img_files[@]:i:n}"
+  git commit -m "Iteratively adding HTML files"
+  git push origin master --force
+done
+
+img_files=(img/*.png) # use your own glob expression
+n=200               # number of files per command line; adjust to taste
+for ((i=0; i < ${#img_files[@]}; i+=n)); do
+  git add "${img_files[@]:i:n}"
+  git commit -m "Iteratively adding HTML files"
+  git push origin master --force
+done
+
+
